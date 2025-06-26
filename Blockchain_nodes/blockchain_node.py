7777d13734ab3715b8e00b3954d1f7ecbc9cb3c2
daoud doudou <daoud.doudou2001@gmail.com>
2025-06-26 23:47:45 +0200

    Add Blockchain class and Flask app with genesis block

---
+++ Blockchain_nodes/blockchain_node.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from time import time
+
+class Blockchain:
+    def __init__(self):
+        self.transactions = []
+        self.chain = []
+        # Create the genesis block 
+        self.cerate_block(0, '00')
+
+    def cerate_block(self, nonce, previous_hash):
+        """ 
+            Add a block of transactions
+        """
+        block = {
+            'block_number': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.transactions(),
+            'nonce' : nonce,
+            'previous_hash': previous_hash
+        }
+        # reset the current list of transactions
+        self.transactions = []
+
+        self.chain.append(block)
+
+# Initialisation
+blockchain = Blockchain()
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5001, type=int, help="port to listen to")
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port, debug=True)
